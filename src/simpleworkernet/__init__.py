98ffe4a1169473fc1ef20949b35aa9b8d35578d4
Андрей Литвинов <busy4beaver@gmail.com>
2026-07-29 08:27:44 +0700

    fix: import CGraph/FNGraph/Topology from utils.topology (not deprecated graph)

--- src/simpleworkernet/__init__.py
+++ src/simpleworkernet/__init__.py
@@ -72,8 +72,7 @@ else:
     from .smartdata.metadata import MetaData, PathSegment, SegmentType
     from .utils.decorators import api_method, logged_method, timer
     from .utils.graphics import save_svg, load_svg, svg_to_png
-    from .utils.graph import CGraph, FNGraph
-    from .utils.topology import Topology
+    from .utils.topology import Topology, CGraph, FNGraph
     from .scripts.uninstall import cleanup_with_confirmation, cleanup
 
     # ------------------------------------------------------------------------
