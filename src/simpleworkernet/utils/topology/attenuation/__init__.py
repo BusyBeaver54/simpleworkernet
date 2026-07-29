30d6bf8bacf48a3155042362985705e0cb365ce0
Андрей Литвинов <busy4beaver@gmail.com>
2026-07-29 21:27:01 +0700

    feat(topology): Attenuation calculator + DataCache inventory/length helpers

---
+++ src/simpleworkernet/utils/topology/attenuation/__init__.py
@@ -0,0 +1,22 @@
+# simpleworkernet/utils/topology/attenuation/__init__.py
+"""
+Расчёт оптических затуханий по CGraph (по запросу, не при build).
+
+    from simpleworkernet.utils.topology.attenuation import Attenuation, AttenuationCatalog
+
+    cat = AttenuationCatalog.with_defaults()
+    att = Attenuation(cgraph, catalog=cat, wavelength=1550)
+    report = att.olt_to_customer(customer_id)
+    print(report.total_db, report.to_table())
+"""
+
+from .catalog import AttenuationCatalog
+from .models import AttenuationSegment, PathReport
+from .calculator import Attenuation
+
+__all__ = [
+    "Attenuation",
+    "AttenuationCatalog",
+    "AttenuationSegment",
+    "PathReport",
+]
