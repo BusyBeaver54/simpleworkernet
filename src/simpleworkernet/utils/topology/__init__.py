04569fbfac34293d254097ddfe721795699c113f
Андрей Литвинов <busy4beaver@gmail.com>
2026-08-09 23:08:20 +0700

    feat: NetworkTopology alias, multipath attenuation, package exports

--- src/simpleworkernet/utils/topology/__init__.py
+++ src/simpleworkernet/utils/topology/__init__.py
@@ -1,77 +1,37 @@
 # simpleworkernet/utils/topology/__init__.py
-"""
-Модульная графовая топология сети WorkerNet.
-
-Публичный API:
-    Topology, CGraph, FNGraph, DataCache, ObjKey, Interface,
-    Attenuation, AttenuationCatalog, PathReport,
-    константы типов объектов (TYPE_*).
-"""
+"""Модульная графовая топология сети WorkerNet."""
 
 from .constants import (
-    TYPE_CUSTOMER,
-    TYPE_FIBER,
-    TYPE_SPLITTER,
-    TYPE_CROSS,
-    TYPE_CWDM,
-    TYPE_SWITCH,
-    TYPE_OLT,
-    TYPE_ONU,
-    TYPE_RADIO,
-    DEVICE_TYPES,
-    SIDE_TYPES,
-    TERMINAL_TYPES,
-    ALL_OBJECT_TYPES,
+    TYPE_CUSTOMER, TYPE_FIBER, TYPE_SPLITTER, TYPE_CROSS, TYPE_CWDM,
+    TYPE_SWITCH, TYPE_OLT, TYPE_ONU, TYPE_RADIO,
+    DEVICE_TYPES, SIDE_TYPES, TERMINAL_TYPES, ALL_OBJECT_TYPES,
 )
+from .errors import TopologyBuildError
 from .keys import ObjKey, Interface
 from .models import CGraphVertex, CGraphEdge, FNGraphVertex, FNGraphEdge
 from .cache import DataCache
 from .graphs import CGraph, FNGraph
 from .topology import Topology
+from .topology_get_linear import apply_network_topology_api
 from .merge import merge_cgraphs, merge_fngraphs
 from .attenuation import (
-    Attenuation,
-    AttenuationCatalog,
-    AttenuationSegment,
-    PathReport,
+    Attenuation, AttenuationCatalog, AttenuationSegment, PathReport, MultiPathReport,
 )
 
+# Topology → NetworkTopology (+ get_linear)
+NetworkTopology = apply_network_topology_api(Topology)
+Topology = NetworkTopology  # BC alias
+
 __all__ = [
-    # constants
-    "TYPE_CUSTOMER",
-    "TYPE_FIBER",
-    "TYPE_SPLITTER",
-    "TYPE_CROSS",
-    "TYPE_CWDM",
-    "TYPE_SWITCH",
-    "TYPE_OLT",
-    "TYPE_ONU",
-    "TYPE_RADIO",
-    "DEVICE_TYPES",
-    "SIDE_TYPES",
-    "TERMINAL_TYPES",
-    "ALL_OBJECT_TYPES",
-    # keys
-    "ObjKey",
-    "Interface",
-    # models
-    "CGraphVertex",
-    "CGraphEdge",
-    "FNGraphVertex",
-    "FNGraphEdge",
-    # cache
-    "DataCache",
-    # graphs
-    "CGraph",
-    "FNGraph",
-    # merge
-    "merge_cgraphs",
-    "merge_fngraphs",
-    # high-level
-    "Topology",
-    # attenuation
-    "Attenuation",
-    "AttenuationCatalog",
-    "AttenuationSegment",
-    "PathReport",
+    "TYPE_CUSTOMER", "TYPE_FIBER", "TYPE_SPLITTER", "TYPE_CROSS", "TYPE_CWDM",
+    "TYPE_SWITCH", "TYPE_OLT", "TYPE_ONU", "TYPE_RADIO",
+    "DEVICE_TYPES", "SIDE_TYPES", "TERMINAL_TYPES", "ALL_OBJECT_TYPES",
+    "TopologyBuildError",
+    "ObjKey", "Interface",
+    "CGraphVertex", "CGraphEdge", "FNGraphVertex", "FNGraphEdge",
+    "DataCache", "CGraph", "FNGraph",
+    "merge_cgraphs", "merge_fngraphs",
+    "NetworkTopology", "Topology",
+    "Attenuation", "AttenuationCatalog", "AttenuationSegment",
+    "PathReport", "MultiPathReport",
 ]
--- src/simpleworkernet/utils/topology/attenuation/__init__.py
+++ src/simpleworkernet/utils/topology/attenuation/__init__.py
@@ -1,75 +1,32 @@
 # simpleworkernet/utils/topology/attenuation/__init__.py
-"""
-Расчёт оптических затуханий по CGraph (по запросу).
-
-Константы типов — из ``simpleworkernet.utils.constants``::
-
-    from simpleworkernet.utils.constants import TYPE_FIBER, TYPE_OLT
-"""
+"""Расчёт оптических затуханий по CGraph."""
 
 from ...constants import (
-    TYPE_CUSTOMER,
-    TYPE_FIBER,
-    TYPE_SPLITTER,
-    TYPE_CROSS,
-    TYPE_CWDM,
-    TYPE_SWITCH,
-    TYPE_OLT,
-    TYPE_ONU,
-    TYPE_RADIO,
-    DEVICE_TYPES,
-    SIDE_TYPES,
-    TERMINAL_TYPES,
-    ALL_OBJECT_TYPES,
+    TYPE_CUSTOMER, TYPE_FIBER, TYPE_SPLITTER, TYPE_CROSS, TYPE_CWDM,
+    TYPE_SWITCH, TYPE_OLT, TYPE_ONU, TYPE_RADIO,
+    DEVICE_TYPES, SIDE_TYPES, TERMINAL_TYPES, ALL_OBJECT_TYPES,
 )
 from .catalog import AttenuationCatalog, guess_ratio_key
 from .models import AttenuationSegment, PathReport
+from .multipath import MultiPathReport
 from .calculator import Attenuation, AttenuationError
 from .template import (
-    attenuation_json_path,
-    client_file_key,
-    generate_template,
-    load_attenuation_catalog,
-    update_template,
+    attenuation_json_path, client_file_key, generate_template,
+    load_attenuation_catalog, update_template,
 )
 from .report_io import (
-    save_path_report,
-    load_path_report,
-    default_reports_dir,
-    report_filename,
+    save_path_report, load_path_report, default_reports_dir, report_filename,
 )
 from .calculator_pairs import pair_plan, validate_pair_inputs, PairPlan
 
 __all__ = [
-    "TYPE_CUSTOMER",
-    "TYPE_FIBER",
-    "TYPE_SPLITTER",
-    "TYPE_CROSS",
-    "TYPE_CWDM",
-    "TYPE_SWITCH",
-    "TYPE_OLT",
-    "TYPE_ONU",
-    "TYPE_RADIO",
-    "DEVICE_TYPES",
-    "SIDE_TYPES",
-    "TERMINAL_TYPES",
-    "ALL_OBJECT_TYPES",
-    "Attenuation",
-    "AttenuationError",
-    "AttenuationCatalog",
-    "AttenuationSegment",
-    "PathReport",
-    "PairPlan",
-    "pair_plan",
-    "validate_pair_inputs",
-    "guess_ratio_key",
-    "attenuation_json_path",
-    "client_file_key",
-    "generate_template",
-    "load_attenuation_catalog",
-    "update_template",
-    "save_path_report",
-    "load_path_report",
-    "default_reports_dir",
-    "report_filename",
+    "TYPE_CUSTOMER", "TYPE_FIBER", "TYPE_SPLITTER", "TYPE_CROSS", "TYPE_CWDM",
+    "TYPE_SWITCH", "TYPE_OLT", "TYPE_ONU", "TYPE_RADIO",
+    "DEVICE_TYPES", "SIDE_TYPES", "TERMINAL_TYPES", "ALL_OBJECT_TYPES",
+    "Attenuation", "AttenuationError", "AttenuationCatalog",
+    "AttenuationSegment", "PathReport", "MultiPathReport",
+    "PairPlan", "pair_plan", "validate_pair_inputs", "guess_ratio_key",
+    "attenuation_json_path", "client_file_key", "generate_template",
+    "load_attenuation_catalog", "update_template",
+    "save_path_report", "load_path_report", "default_reports_dir", "report_filename",
 ]
